6b9194e86b522331f17d938b5d96f00f24584a8b
hooyn <hooyn_industry@naver.com>
2022-07-09 18:59:28 +0900

    Create Response.java [API 응답을 위한 클래스]

--- src/main/java/hooyn/todo/TodoApplication.java
+++ src/main/java/hooyn/todo/TodoApplication.java
@@ -1,10 +1,14 @@
 package hooyn.todo;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -12,9 +16,14 @@ import java.time.format.DateTimeFormatter;
 public class TodoApplication {
 	static Logger logger = LoggerFactory.getLogger(TodoApplication.class);
 	public static void main(String[] args) {
-		logger.info("서버 정상 작동..." + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-
+		logger.info("서버 정상 작동...  시작시간 ["
+				+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+				+" ]");
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em){
+		return new JPAQueryFactory(em);
+	}
 }
