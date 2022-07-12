417181a5eb380e77a920b1ca8f23165f02201288
hooyn <hooyn_industry@naver.com>
2022-07-12 22:51:37 +0900

    Update TodoApplication.java [로깅 작업]

--- src/main/java/hooyn/todo/TodoApplication.java
+++ src/main/java/hooyn/todo/TodoApplication.java
@@ -1,24 +1,21 @@
 package hooyn.todo;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import hooyn.todo.service.DateService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
+@Slf4j
 public class TodoApplication {
-	static Logger logger = LoggerFactory.getLogger(TodoApplication.class);
 	public static void main(String[] args) {
-		logger.info("서버 정상 작동...  시작시간 ["
-				+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-				+" ]");
+		log.info("서버 정상 작동 8080..." + new DateService().getDate());
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
