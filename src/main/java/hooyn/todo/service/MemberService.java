7fc93f3f1aebc996c60a11a6deae1aa26790afad
hooyn <hooyn_industry@naver.com>
2022-07-15 22:42:29 +0900

    관심사 분리를 통한 DIP 완성 [MemberService와 MemberRepository 인터페이스 생성, MEmberServiceImpl, MemberRepositoryImpl 생성]

--- src/main/java/hooyn/todo/service/MemberService.java
+++ src/main/java/hooyn/todo/service/MemberService.java
@@ -1,81 +1,28 @@
 package hooyn.todo.service;
 
 import hooyn.todo.domain.Member;
-import hooyn.todo.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 import java.util.regex.Pattern;
 
-@Service
-@RequiredArgsConstructor
-public class MemberService {
+public interface MemberService {
 
-    private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
+    //회원가입
+    UUID join(String userNM, String userID, String userPW);
 
-    /**
-     * 회원 가입
-     */
-    @Transactional
-    public UUID join(String userNM, String userID, String userPW){
-        Member member = new Member(userNM, userID, userPW);
-        member.encodePassword(passwordEncoder);
-        return memberRepository.save(member);
-    }
+    //UserID를 통한 회원(엔티티) 검색
+    Member findUserByUserID(String userID);
 
-    /**
-     * 아이디에 따른 회원 조회
-     */
-    @Transactional(readOnly = true)
-    public Member findUserByUserID(String userID){
-        return memberRepository.findByUserId(userID);
-    }
+    //UUID를 통한 회원(엔티티) 검색
+    Member findUserByUUID(String uuid);
 
-    /**
-     * UUID에 따른 회원 조회
-     */
-    @Transactional(readOnly = true)
-    public Member findUserByUUID(String uuid){
-        return memberRepository.findOne(uuid);
-    }
+    //ID 중복확인
+    boolean checkDuplicatedID(String userID);
 
-    /**
-     * 아이디 중복 확인
-     */
-    @Transactional(readOnly = true)
-    public boolean checkDuplicatedID(String userID){
-        Member member = memberRepository.findByUserId(userID);
-        if(member!=null){
-            return false; //아이디로 검색되는 회원이 있다면 중복(false)
-        } else return true;
-    }
+    //비밀번호 제약조건 확인
+    boolean checkPasswordConstraint(String userPW);
 
-    /**
-     * 비밀번호 제약조건 확인
-     */
-    public boolean checkPasswordConstraint(String userPW){
-        Pattern pattern = Pattern.compile("^(?=.*[a-zA-Z])(?=.*\\d).{8,20}$");
-        return pattern.matcher(userPW).matches();
-    }
-
-    /**
-     * 비밀번호 변경 (변경 감지 이용)
-     */
-    @Transactional
-    public UUID changePassword(String uuid, String userPW){
-        Member member = findUserByUUID(uuid);
-        if(member==null) return null; //회원 정보 없음
-
-        //비밀번호 암호화
-        String encodedPW = passwordEncoder.encode(userPW);
-
-        //변경 감지를 이용한 사용자 비밀번호 업데이트
-        member.setUserPW(encodedPW);
-
-        return member.getUuid();
-    }
+    //비밀번호 변경
+    UUID changePassword(String uuid, String userPW);
 }
---
+++ src/main/java/hooyn/todo/service/MemberServiceImpl.java
@@ -0,0 +1,85 @@
+package hooyn.todo.service;
+
+import hooyn.todo.domain.Member;
+import hooyn.todo.repository.MemberRepository;
+import hooyn.todo.repository.MemberRepositoryImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+@Service
+@RequiredArgsConstructor
+@Primary //MemberService 의존관계 주입 시 우선권을 가진다.
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional
+    public UUID join(String userNM, String userID, String userPW){
+        Member member = new Member(userNM, userID, userPW);
+        member.encodePassword(passwordEncoder);
+        return memberRepository.save(member);
+    }
+
+    /**
+     * 아이디에 따른 회원 조회
+     */
+    @Transactional(readOnly = true)
+    public Member findUserByUserID(String userID){
+        return memberRepository.findByUserId(userID);
+    }
+
+    /**
+     * UUID에 따른 회원 조회
+     */
+    @Transactional(readOnly = true)
+    public Member findUserByUUID(String uuid){
+        return memberRepository.findOne(uuid);
+    }
+
+    /**
+     * 아이디 중복 확인
+     */
+    @Transactional(readOnly = true)
+    public boolean checkDuplicatedID(String userID){
+        Member member = memberRepository.findByUserId(userID);
+        if(member!=null){
+            return false; //아이디로 검색되는 회원이 있다면 중복(false)
+        } else return true;
+    }
+
+    /**
+     * 비밀번호 제약조건 확인
+     */
+    public boolean checkPasswordConstraint(String userPW){
+        Pattern pattern = Pattern.compile("^(?=.*[a-zA-Z])(?=.*\\d).{8,20}$");
+        return pattern.matcher(userPW).matches();
+    }
+
+    /**
+     * 비밀번호 변경 (변경 감지 이용)
+     */
+    @Transactional
+    public UUID changePassword(String uuid, String userPW){
+        Member member = findUserByUUID(uuid);
+        if(member==null) return null; //회원 정보 없음
+
+        //비밀번호 암호화
+        String encodedPW = passwordEncoder.encode(userPW);
+
+        //변경 감지를 이용한 사용자 비밀번호 업데이트
+        member.setUserPW(encodedPW);
+
+        return member.getUuid();
+    }
+}
