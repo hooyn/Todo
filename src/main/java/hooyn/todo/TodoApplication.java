9b1f0b182e745840e7da364fa3b8dc8a008459d7
hooyn <hooyn_industry@naver.com>
2022-07-15 22:22:12 +0900

    QueryDSL 설정

--- src/main/java/hooyn/todo/TodoApplication.java
+++ src/main/java/hooyn/todo/TodoApplication.java
@@ -1,13 +1,9 @@
 package hooyn.todo;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import hooyn.todo.service.DateService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.persistence.EntityManager;
 
 @SpringBootApplication
 @Slf4j
@@ -15,10 +11,5 @@ public class TodoApplication {
 	public static void main(String[] args) {
 		log.info("서버 정상 작동 8080..." + new DateService().getDate());
 		SpringApplication.run(TodoApplication.class, args);
-	}
-
-	@Bean
-	JPAQueryFactory jpaQueryFactory(EntityManager em){
-		return new JPAQueryFactory(em);
 	}
 }
