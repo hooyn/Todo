bcc5aba5066bbaac0bdbba0f877bb9204bb1dc05
hooyn <hooyn_industry@naver.com>
2022-07-18 08:44:16 +0900

    Create Request Form

---
+++ src/main/java/hooyn/todo/api/request/todo/UpdateTodoRequest.java
@@ -0,0 +1,14 @@
+package hooyn.todo.api.request.todo;
+
+import hooyn.todo.domain.Deadline;
+import lombok.Getter;
+
+@Getter
+public class UpdateTodoRequest {
+    private String uuid;
+    private Long todo_id;
+    private String title;
+    private String content;
+    private Deadline deadline;
+
+}
