7a6eb3d9ea424983429a4b1bd1bbe1ae8bd969d9
hooyn <hooyn_industry@naver.com>
2022-07-27 19:45:12 +0900

    Update Gitignore [add ./logs]

--- src/main/java/hooyn/todo/api/request/todo/DeleteTodoRequest.java
+++
@@ -1,9 +0,0 @@
-package hooyn.todo.api.request.todo;
-
-import lombok.Getter;
-
-@Getter
-public class DeleteTodoRequest {
-    private String uuid;
-    private Long todo_id;
-}
--- src/main/java/hooyn/todo/api/request/todo/UpdateTodoStatusRequest.java
+++
@@ -1,9 +0,0 @@
-package hooyn.todo.api.request.todo;
-
-import lombok.Getter;
-
-@Getter
-public class UpdateTodoStatusRequest {
-    private String uuid;
-    private Long todo_id;
-}
