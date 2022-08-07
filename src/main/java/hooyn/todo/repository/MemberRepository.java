a9a05fa33d557c884fd03695875164db7faff2ed
hooyn <hooyn_industry@naver.com>
2022-08-07 19:34:37 +0900

    Refactoring [관습적인 추상화 제거]

--- src/main/java/hooyn/todo/repository/MemberRepository.java
+++ src/main/java/hooyn/todo/repository/MemberRepository.java
@@ -1,17 +1,43 @@
 package hooyn.todo.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import hooyn.todo.domain.Member;
+import hooyn.todo.domain.QMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
 import java.util.UUID;
 
-public interface MemberRepository {
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
 
-    //회원 정보 저장
-    UUID save(Member member);
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
 
-    //회원 엔티티 조회
-    Member findByUUID(String uuid);
+    /**
+     * 회원 정보 저장
+     */
+    public UUID save(Member member){
+        em.persist(member);
+        return member.getUuid();
+    }
 
-    //사용자 아이디에 따른 회원 조회
-    Member findByUserId(String userID);
+    /**
+     * 회원 엔티티 조회
+     */
+    public Member findByUUID(String uuid){
+        return em.find(Member.class, UUID.fromString(uuid));
+    }
+
+    /**
+     * UserID에 따른 회원 엔티티 조회
+     */
+    public Member findByUserId(String userID){
+        return queryFactory
+                .selectFrom(QMember.member)
+                .where(QMember.member.userID.eq(userID))
+                .fetchOne();
+    }
 }
--- src/main/java/hooyn/todo/repository/TodoRepository.java
+++ src/main/java/hooyn/todo/repository/TodoRepository.java
@@ -1,34 +1,122 @@
 package hooyn.todo.repository;
 
 import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import hooyn.todo.domain.Deadline;
 import hooyn.todo.domain.Todo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.UUID;
 
-public interface TodoRepository {
+import static hooyn.todo.domain.QTodo.todo;
 
-    //투두 작성
-    Long save(Todo todo);
+@Repository
+@RequiredArgsConstructor
+public class TodoRepository {
 
-    //투두 엔티티 검색
-    Todo findById(Long id);
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
 
-    //투두 UUID에 따른 엔티티 검색
-    List<Todo> findByUUID(String uuid);
+    /**
+     * 투두 작성
+     */
+    public Long save(Todo todo) {
+        em.persist(todo);
 
-    //투두 Deadline에 따른 엔티티 검색
-    List<Todo> findByDeadline(String uuid, Deadline deadLine, Integer page);
+        return todo.getId();
+    }
 
-    //투두 콘텐츠 키워드에 따른 엔티티 검색
-    List<Todo> findByContent(String uuid, String content, Integer page);
+    /**
+     * 투두_아이디에 따른 투두 조회
+     */
+    public Todo findById(Long id) {
+        return em.find(Todo.class, id);
+    }
 
-    //투두 Year, Month 받아서 String으로 투두 있는 날짜 반환
-    List<Tuple> findEventByYearMonth(String uuid, String year, String month);
+    /**
+     * UUID에 따른 투두 조회
+     */
+    public List<Todo> findByUUID(String uuid) {
+        return queryFactory
+                .selectFrom(todo)
+                .where(todo.member.uuid.eq(UUID.fromString(uuid)))
+                .fetch();
+    }
 
-    //투두 수정, 삭제 권한 확인
-    boolean checkAuthorization(String uuid, Long id);
+    /**
+     * 기한에 따른 투두 조회
+     */
+    public List<Todo> findByDeadline(String uuid, Deadline deadLine, Integer page) {
+        return queryFactory
+                .selectFrom(todo)
+                .where(todo.member.uuid.eq(UUID.fromString(uuid))
+                        .and(todo.deadline.date.eq(deadLine.getDate())))
+                .orderBy(todo.create_time.desc())
+                .offset(0+((page-1)*10))
+                .limit(10)
+                .fetch();
+    }
 
-    //투두 삭제
-    Long delete(Long id);
+    /**
+     * content가 포함된 투두 조회
+     */
+    public List<Todo> findByContent(String uuid, String content, Integer page) {
+        return queryFactory
+                .selectFrom(todo)
+                .where(
+                        (todo.content.like("%"+content+"%").or(todo.title.like("%"+content+"%")))
+                                .and(todo.member.uuid.eq(UUID.fromString(uuid)))
+                )
+                .orderBy(todo.create_time.desc())
+                .offset(0+((page-1)*10))
+                .limit(10)
+                .fetch();
+    }
+
+    public List<Tuple> findEventByYearMonth(String uuid, String year, String month){
+        List<Tuple> result = queryFactory
+                .select(todo.deadline.date, todo.deadline.date.count())
+                .distinct()
+                .from(todo)
+                .where(todo.deadline.date.like(year + "/" + month + "/%")
+                        .and(todo.member.uuid.eq(UUID.fromString(uuid))))
+                .groupBy(todo.deadline.date)
+                .fetch();
+
+        return result;
+    }
+
+
+    /**
+     * 권한 확인
+     */
+    public boolean checkAuthorization(String uuid, Long id) {
+        List<Todo> fetch = queryFactory
+                .selectFrom(todo)
+                .where(todo.member.uuid.eq(UUID.fromString(uuid))
+                        .and(todo.id.eq(id)))
+                .fetch();
+
+        if(fetch.size()>0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 투두 삭제
+     */
+    public Long delete(Long id) {
+        queryFactory
+                .delete(todo)
+                .where(todo.id.eq(id))
+                .execute();
+
+        return id;
+    }
+
 }
